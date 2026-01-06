e9aff252132bee15b630355ef576695f302b50cf
kinzokudev <kin@kinzoku.dev>
2026-01-06 16:20:07 -0500

    mod,nixos(gui/niri,default): add session variables for wayland
    Signed-off-by: kinzokudev <kin@kinzoku.dev>

--- modules/nixos/gui/niri/default.nix
+++ modules/nixos/gui/niri/default.nix
@@ -7,4 +7,12 @@
     enable = true;
     withUWSM = true;
   };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_LAUNCH_FLAGS = "--enable-wayland-ime --wayland-text-input-version=3 --enable-features=WaylandLinuxDrmSyncobj";
+    WEBKIT_DISABLE_DMABUF_RENDERER = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    XDG_SESSION_TYPE = "wayland";
+  };
 }
