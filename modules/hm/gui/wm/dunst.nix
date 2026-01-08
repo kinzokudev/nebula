4ad2ba6570fd931d880977848d5f7c8381a712f3
kinzokudev <kin@kinzoku.dev>
2026-01-07 19:22:45 -0500

    mod,hm(gui/wm,dunst): more dunst config
    Signed-off-by: kinzokudev <kin@kinzoku.dev>

--- modules/hm/gui/wm/dunst.nix
+++ modules/hm/gui/wm/dunst.nix
@@ -1,6 +1,39 @@
 {pkgs, ...}: {
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        enable_recursive_icon_lookup = true;
+        ellipsize = "end";
+        follow = "mouse";
+        font = "JetBrainsMono Nerd Font 12";
+        # frame_color = "#b7bdf8";
+        # frame_width = 2;
+        # background = "#24273a";
+        # foreground = "#cad3f5";
+      };
+
+      urgency_low = {
+        frame_width = 1;
+        frame_color = "#b7bdf8";
+        background = "#24273a";
+        foreground = "#cad3f5";
+      };
+
+      urgency_normal = {
+        frame_width = 2;
+        frame_color = "#b7bdf8";
+        background = "#24273a";
+        foreground = "#cad3f5";
+      };
+
+      urgency_critical = {
+        frame_width = 2;
+        frame_color = "#ff2550";
+        background = "#24273a";
+        foreground = "#cad3f5";
+      };
+    };
   };
 
   home.packages = [
