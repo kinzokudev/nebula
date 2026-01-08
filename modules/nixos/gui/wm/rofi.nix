c3bf28345062aaaa0f609df1d6aa64df6cbd4be6
kinzokudev <kin@kinzoku.dev>
2026-01-08 12:37:43 -0500

    mod,nixos(gui/wm,rofi): add clipboard config
    Signed-off-by: kinzokudev <kin@kinzoku.dev>

--- modules/nixos/gui/wm/rofi.nix
+++ modules/nixos/gui/wm/rofi.nix
@@ -41,6 +41,7 @@
         display-window: " ";
         display-emoji: " ";
         display-calc: " ";
+        display-clipboard: "󰨸 ";
         drun-display-format: "{name} [<span weight='light' size='small'><i>({generic})</i></span>]";
         window-format: "{w} 󰧟 {c} 󰧟 {t}";
         // display-filebrowser: "󰉖";
@@ -723,6 +724,149 @@
         reverse: false;
         fixed-height: true;
         fixed-columns: false;
+
+        spacing: 5px;
+        margin: 0px;
+        padding: 0px;
+        background-color: transparent;
+        text-color: @fg-color;
+        cursor: "default";
+      }
+      scrollbar {
+        handle-width: 4px;
+        handle-color: @fg-special;
+        border-radius: 4px;
+        background-color: @bg-alt;
+      }
+      element {
+        enabled: true;
+        spacing: 10px;
+        margin: 0px;
+        padding: 5px 10px;
+        border-radius: 6px;
+        background-color: transparent;
+        text-color: @fg-color;
+        cursor: pointer;
+      }
+      element normal.normal {
+        background-color: @bg-color;
+        text-color: @fg-color;
+      }
+      element normal.urgent {
+        background-color: @bg-color;
+        text-color: @red;
+      }
+      element normal.active {
+        background-color: @bg-color;
+        text-color: @green;
+      }
+      element selected.normal {
+        background-color: @bg-alt;
+        text-color: @fg-color;
+      }
+      element selected.urgent {
+        background-color: @bg-alt;
+        text-color: @red;
+      }
+      element selected.active {
+        background-color: @bg-alt;
+        text-color: @green;
+      }
+      element alternate.normal {
+        background-color: @bg-color;
+        text-color: @fg-color;
+      }
+      element alternate.urgent {
+        background-color: @bg-color;
+        text-color: @red;
+      }
+      element alternate.active {
+        background-color: @bg-color;
+        text-color: @green;
+      }
+      element-icon {
+        background-color: transparent;
+        text-color: inherit;
+        size: 20px;
+        cursor: inherit;
+      }
+      element-text {
+        background-color: transparent;
+        text-color: inherit;
+        highlight: inherit;
+        cursor: inherit;
+        vertical-align: 0.5;
+        horizontal-align: 0.0;
+      }
+    '';
+    "rofi/clipboard.rasi".text = ''
+      @import "globals"
+
+      window {
+        enabled: true;
+        width: 800px;
+        border-radius: 8px;
+        border: 2px solid;
+        border-color: @bd-color;
+        padding: 0px;
+        location: center;
+        anchor: center;
+        fullscreen: false;
+        x-offset: 0px;
+        y-offset: 0px;
+        margin: 0px;
+        cursor: "default";
+        background-color: @bg-color;
+      }
+      mainbox {
+        enabled: true;
+        spacing: 10px;
+        margin: 0px;
+        padding: 10px 8px;
+        background-color: transparent;
+        children: ["inputbar", "listview" ];
+      }
+      inputbar {
+        enabled: true;
+        spacing: 0px;
+        margin: 0px;
+        padding: 0px;
+        background-color: transparent;
+        text-color: @fg-color;
+        children: ["prompt",  "entry"];
+      }
+
+      prompt {
+        enabled: true;
+        background-color: inherit;
+        text-color: inherit;
+        font: "JetBrainsMono Nerd Font 24";
+        margin: 0px 0px 0px 8px;
+        padding: 0px;
+      }
+      entry {
+        enabled: true;
+        background-color: inherit;
+        text-color: inherit;
+        cursor: text;
+        placeholder: "[CLIPBOARD HISTORY...]";
+        placeholder-color: @fg-subtle;
+        margin: 0px;
+        padding: 10px 0px;
+      }
+
+
+      listview {
+        enabled: true;
+        columns: 1;
+        lines: 10;
+        cycle: true;
+        dynamic: true;
+        scrollbar: true;
+        layout: vertical;
+        reverse: false;
+        fixed-height: true;
+        fixed-columns: true;
 
         spacing: 5px;
         margin: 0px;
