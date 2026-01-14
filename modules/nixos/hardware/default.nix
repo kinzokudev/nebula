a85d557d078405ec37a7daee455015ec14e271c7
kinzokudev <kin@kinzoku.dev>
2026-01-14 15:18:54 -0500

    mod,nixos(hardware): add openrgb
    Signed-off-by: kinzokudev <kin@kinzoku.dev>

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -3,5 +3,6 @@
     ./audio.nix
     ./bluetooth.nix
     ./keyd.nix
+    ./openrgb.nix
   ];
 }
---
+++ modules/nixos/hardware/openrgb.nix
@@ -0,0 +1,7 @@
+{pkgs, ...}: {
+  services.hardware.openrgb = {
+    enable = true;
+    motherboard = "amd";
+    package = pkgs.openrgb-with-all-plugins;
+  };
+}
