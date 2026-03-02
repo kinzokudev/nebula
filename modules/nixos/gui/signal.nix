eb37df09e9b1555ca3c82f57791316d3cd9134ff
kinzokudev <kin@kinzoku.dev>
2026-03-02 01:52:12 -0500

    mod,nixos(gui/signal): override desktop item to add password store exec flag
    Signed-off-by: kinzokudev <kin@kinzoku.dev>

--- modules/nixos/gui/signal.nix
+++ modules/nixos/gui/signal.nix
@@ -1,5 +1,28 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    signal-desktop # E2EE messenger app
+    # E2EE messenger app
+    (signal-desktop.overrideAttrs {
+      desktopItems = [
+        (pkgs.makeDesktopItem {
+          name = "signal";
+          desktopName = "Signal";
+          exec = "signal-desktop --password-store=\"kwallet6\"";
+          icon = "signal-desktop";
+          type = "Application";
+          terminal = false;
+          comment = "Private messaging from your desktop";
+          startupWMClass = "signal";
+          mimeTypes = [
+            "x-scheme-handler/sgnl"
+            "x-scheme-handler/signalcaptcha"
+          ];
+          categories = [
+            "Network"
+            "InstantMessaging"
+            "Chat"
+          ];
+        })
+      ];
+    })
   ];
 }
